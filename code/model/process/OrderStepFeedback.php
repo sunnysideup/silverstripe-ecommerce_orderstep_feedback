8779054f7fc428750303f15b5bed1fc3455fa392
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-06-25 14:15:29 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/model/process/OrderStepFeedback.php
+++ code/model/process/OrderStepFeedback.php
@@ -35,6 +35,20 @@ class OrderStepFeedback extends OrderStep
      */
     protected $emailClassName = "OrderStepFeedback_Email";
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'OrderStepFeedback';
+
     private static $db = array(
         'SendFeedbackEmail' => 'Boolean',
         'MinDays' => 'Int',
